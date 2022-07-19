647da6e043cce408a54513ff4ea69a13aa27ec37
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-07-19 03:16:40 +0200

    Modif apiMW

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -1,12 +1,20 @@
 import {addRecipes, LOAD_RECIPES} from "../actions/recipes";
 import axios from "axios";
 
-const apiMW = (store) => (next) => async (action) =>{
+const apiMW = (store) => (next) => (action) =>{
     switch (action.type) {
         case LOAD_RECIPES:
-            const response = await axios.get('http://localhost:3001/recipes');
-            const recipes = response.data;
-            store.dispatch(addRecipes(recipes))
+        const loadData = async () => {
+           try {
+                const response = await axios.get('http://localhost:3001/recipes');
+                const recipes = response.data;
+                store.dispatch(addRecipes(recipes));
+            }
+            catch (e) {
+                console.log(e)
+            }
+        };
+        loadData();
             break;
 
         default:
